9703ed101381aebb5bbd55e7120a4a18a5aa8e6f
Anvesh <anveshyadavmokam@gmail.com>
2025-03-24 09:06:34 +0530

    cors removed

--- vyuha-backend/server.ts
+++ vyuha-backend/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import authRoute from "./routes/authRoute";
 import userRouter from "./routes/userRoute";
 import instructorRoute from "./routes/instructorRoute";
@@ -7,15 +6,6 @@ import adminRoute from "./routes/adminRoute";
 import mentorRoute from "./routes/mentorRoute";
 const port = process.env.PORT || 9000;
 const app = express();
-const corsOptions = {
-    origin: [
-        'https://project-v-seven.vercel.app',
-        'http://localhost:3000'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'token']
-};
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRoute);
